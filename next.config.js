02b94c93e6e8c54f2de30550e5bce883991a2026
alaataha <alaataha@alaatahas-Mac-Pro.local>
2025-11-16 04:38:08 -0800

    Fix build: remove optimizeCss experimental feature

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-  experimental: {
-    optimizeCss: true,
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
